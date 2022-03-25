edba8875cb1f9eb6f2f00e7a6239627287333e6d
kotoriのねこ <2567702743@qq.com>
2022-03-25 06:41:01 +0000

    [chore] refactor user tile

--- components/userTile.tsx
+++ components/userTile.tsx
@@ -8,8 +8,25 @@ interface UserTileProps {
 const UserTile = (props: UserTileProps): JSX.Element => {
   const { user } = props
   return (
-    <div>
-      <span>{ user.username }</span>
+    <div className="flex flex-col gap-4 w-full">
+      {/* Avatar */}
+      <div className="rounded-full w-32 md:w-full aspect-w-16 aspect-h-16 md:max-w-48 bg-white shadow-md">
+      </div>
+      {/* Info */}
+      <div className="flex flex-col gap-2 w-full whitespace-nowrap">
+        <span className="font-medium text-2xl tracking-wide">{user.username}</span>
+        <span className="font-thin text-xl tracking-wide">{user.name}</span>
+        <span className="text-md">{user.email}</span>
+      </div>
+      {/* Website */}
+      <div>
+        <a
+          href={user.website}
+          target="_blank noopener noreferrer"
+        >
+          {user.website}
+        </a>
+      </div>
     </div>
   )
 }
